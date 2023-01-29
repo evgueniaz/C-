6bbf9fe6d10bd756b7ba5998cf531bc920720a41
Evguenia Zanina <evgueniaz@hotmail.com>
2023-01-29 03:34:02 +0300

    Homework #1 Ex #6

---
+++ HW_1_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Enter a whole number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.Write("Your number is even");
+}
+else
+{
+    Console.Write("Your number is odd");
+}
+
