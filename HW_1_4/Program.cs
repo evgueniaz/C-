80c09e71953fddc313d2a32a88b9f0c621659673
Evguenia Zanina <evgueniaz@hotmail.com>
2023-01-29 03:34:33 +0300

    Homework #1 Ex #8

---
+++ HW_1_4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Write("Enter a natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+if(number <= 1)
+{
+    Console.Write("There are no positive even numbers less than your number");
+}
+else
+{
+    while(count <= number - 2)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+        count += 2;
+    }
+    Console.Write(count);
+}
+
+
