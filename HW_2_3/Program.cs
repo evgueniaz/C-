08405f60330cf58b35dd174469fa4ecc2b1cbde9
Evguenia Zanina <evgueniaz@hotmail.com>
2023-01-30 23:12:26 +0300

    Homework #2 Ex #3

---
+++ HW_2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Enter a number corresponding to a day of the week from 1 to 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <1 || number > 7) Console.WriteLine("You've entered a number out of the interval");
+else if (number == 6 || number == 7) Console.WriteLine("Yes, it's weekend!");
+else
+{
+    Console.WriteLine("No, it's a working day...");
+}
