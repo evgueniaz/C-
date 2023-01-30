46143595fe23b1f520887a216db270edb80323d2
Evguenia Zanina <evgueniaz@hotmail.com>
2023-01-30 23:11:45 +0300

    Homework #2 Ex #2

---
+++ HW_2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 && number > -100) Console.WriteLine($"There's no third digit!");
+else
+{
+    if (number < 0) number = number * (-1);
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine($"The third digit of your number from the left is {result}");
+}
