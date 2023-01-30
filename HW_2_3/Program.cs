858d8013ea73d951be96527a3d161b8e81acb620
Evguenia Zanina <evgueniaz@hotmail.com>
2023-01-30 23:36:45 +0300

    Homework #2 Ex #3 extended

--- HW_2_3/Program.cs
+++ HW_2_3/Program.cs
@@ -22,7 +22,7 @@
 Console.Write("Enter a number corresponding to a day of the week from 1 to 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number < 1) Console.WriteLine("You've entered a number out of the interval");
-else if (number % 7 == 6 || number % 7 + 7 == 7) Console.WriteLine("Yes, it's weekend!");
+else if (number % 7 == 6 || number % 7 == 0) Console.WriteLine("Yes, it's weekend!");
 else
 {
     Console.WriteLine("No, it's a working day...");
