3d59d9ecd0935aa6934c0b1167cb6905d158ee15
Evguenia Zanina <evgueniaz@hotmail.com>
2023-02-05 23:17:03 +0300

    Added Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+Console.WriteLine("Enter a whole number greater than 1: ");
+int num = int.Parse(Console.ReadLine());
+int x = 1;
+while (x <= num)
+{
+    double result = Math.Pow(x, 3);
+    Console.Write($"{result}  ");
+    x += 1;
+}
