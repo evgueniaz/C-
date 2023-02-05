503e930cf1d7ba5e6945d4d07cdb13db84c0ff1a
Evguenia Zanina <evgueniaz@hotmail.com>
2023-02-05 23:29:17 +0300

    Added HW_3_2

---
+++ HW_3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Enter x-coordinate of the first point: ");
+float x_1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter y-coordinate of the first point: ");
+float y_1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter z-coordinate of the first point: ");
+float z_1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter x-coordinate of the second point: ");
+float x_2 = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter y-coordinate of the second point: ");
+float y_2 = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter z-coordinate of the second point: ");
+float z_2 = float.Parse(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow((x_2 - x_1), 2) + Math.Pow((y_2 - y_1), 2) + Math.Pow((z_2 - z_1), 2));
+Console.WriteLine($"Distance between these points equals {distance}");
---
+++ HW_3_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int num;
+while (true)
+{
+    Console.WriteLine("Enter a whole number greater than 1: ");
+    num = int.Parse(Console.ReadLine());
+    if (num > 1) 
+    {
+        break;
+    }
+    Console.WriteLine("You've entered a number that is not greater than 1!!!");  
+}
+
+int x = 1;
+while (x <= num)
+{
+    double result = Math.Pow(x, 3);
+    Console.Write($"{result}  ");
+    x += 1;
+}
