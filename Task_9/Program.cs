289be4d68144240ee91c8fee029feff72f2dcaa5
Evguenia Zanina <evgueniaz@hotmail.com>
2023-02-05 23:16:16 +0300

    Added Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int quartil = int.Parse(Console.ReadLine());
+if(quartile < 1 || quartil > 4) Console.WriteLine("Введен некорректный номер четверти!");
+if(quartile == 1) Console.WriteLine("x > 0, y > 0");
+else if(quartile == 2) Console.WriteLine("x < 0, y > 0");
+else if(quartile == 3) Console.WriteLine("x < 0, y < 0");
+else if (quartile == 4) Console.WriteLine("x > 0, y < 0");
+
